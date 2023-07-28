4035526b868858bd23f0caf101bd74e9aaad15e5
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-07-28 15:51:34 -0400

    Instalacion de dependencias, conexion a BD Mongoose,

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 //Importar express 
 import express  from "express";
+import morgan from "morgan";
 //Inicializa express 
 const app = express()
+
+app.use(morgan('dev')); 
 
 export default app;
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 import app from './app.js'; 
+import {connectDB} from './db.js'
+import colors from 'colors'; 
 
-app.listen(3000)
-console.log('Server on port', 3000)
+connectDB()
+app.listen(4000)
+console.log('Server on port'.bgGreen, 4000)
