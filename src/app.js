4c37e6146040a6ae49adcb8461cd477040479296
Niag2021 <arancibia.guerrero.nicolas@gmail.com>
2023-08-05 22:08:32 -0400

    actualizaciones

--- src/app.js
+++ src/app.js
@@ -1,9 +1,7 @@
-//Importar express 
-import express  from "express";
-import morgan from "morgan";
-//Inicializa express 
+import express from 'express'
+import morgan from 'morgan'
+
 const app = express()
 
-app.use(morgan('dev')); 
-
-export default app;
+app.use(morgan('dev'))
+export default app
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import app from './app.js'; 
-import {connectDB} from './db.js'
+//import {connectDB} from './db.js'
 import colors from 'colors'; 
 
-connectDB()
+//connectDB()
 app.listen(4000)
-console.log('Server on port'.bgGreen, 4000)
+console.log('Server on port'.bgGreen.white, 4000)
